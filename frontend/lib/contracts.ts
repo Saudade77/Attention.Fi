405e9db7eb1b5ba5997300c5b69ebc287fc336e4
Luo Jiayu <13727082343@163.com>
2026-01-29 15:14:59 +0800

    display same data on different browsers

--- frontend/lib/contracts.ts
+++
@@ -1,5 +0,0 @@
-import CreatorFactoryABI from "./CreatorFactory.json";
-import CreatorTokenABI from "./CreatorToken.json";
-
-export const FactoryABI = CreatorFactoryABI.abi;
-export const TokenABI = CreatorTokenABI.abi;
